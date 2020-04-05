3fb0c89cf5c5fc7e7a33268cfde31d20df98cf8b
DreamsOfImran <ibasha66@gmail.com>
2020-04-05 18:13:11 +0530

    Updates

---
+++ precache-manifest.b8e360d6d972eef4539bb848bbf75ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf4776a537a5cd09337b0a183fb659d",
+    "url": "/star-match/index.html"
+  },
+  {
+    "revision": "0c76b18349fec3612a8b",
+    "url": "/star-match/static/css/main.c2955bda.chunk.css"
+  },
+  {
+    "revision": "fe4d61786f3a847faaac",
+    "url": "/star-match/static/js/2.59fd0774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-match/static/js/2.59fd0774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c76b18349fec3612a8b",
+    "url": "/star-match/static/js/main.f230af3e.chunk.js"
+  },
+  {
+    "revision": "9684faeaef795118af1f",
+    "url": "/star-match/static/js/runtime-main.84345c67.js"
+  }
+]);
